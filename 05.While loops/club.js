fb4ef619e3fe097c74afb2941f8d92dea75a8707
Edmont Budakyan <budakyan@gmail.com>
2022-03-14 08:44:01 +0200

    added club

--- 05.While loops/club.js
+++ 05.While loops/club.js
@@ -1,6 +1,33 @@
 function club(input) {
-  //
+  let index = 0;
+  const neededProfit = Number(input[index]);
+  index++;
+  let command = input[index];
+  index++;
+  let income = 0;
+  while (command !== "Party!") {
+    let tempDrink = command;
+    let tempCountsOfDrinks = Number(input[index]);
+    index++;
+    let tempPrice = 0;
+    tempPrice = tempDrink.length * tempCountsOfDrinks;
+    if (tempPrice % 2 !== 0) {
+      tempPrice -= tempPrice * 0.25;
+    }
+    income += tempPrice;
+    if (income >= neededProfit) {
+      console.log(`Target acquired.`);
+      console.log(`Club income - ${income.toFixed(2)} leva.`);
+      break;
+    }
+    command = input[index];
+    index++;
+  }
+  if (command === "Party!") {
+    console.log(`We need ${(neededProfit - income).toFixed(2)} leva more.`);
+    console.log(`Club income - ${income.toFixed(2)} leva.`);
+  }
 }
 
 club(["500", "Bellini", "6", "Bamboo", "7", "Party!"]);
-club(["100", "Sidecar", "7", "Mojito", "5", "White", "Russian", "10"]);
+club(["100", "Sidecar", "7", "Mojito", "5", "White Russian", "10"]);
