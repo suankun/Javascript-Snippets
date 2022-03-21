e08bc9167e56dcc15e59779dcd4102d27ce15544
Edmont Budakyan <budakyan@gmail.com>
2022-03-21 22:56:51 +0200

    added sort number

---
+++ 07.Basic Syntax, Conditional Statements and Loops/sortNumbers.js
@@ -0,0 +1,9 @@
+function sortNumbers(num1, num2, num3) {
+  let arr = [num1, num2, num3];
+  arr.sort((a, b) => b - a);
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+
+sortNumbers(2, 1, 3);
