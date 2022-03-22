58c044da9c950954867324ecce38cb5156540e1c
Edmont Budakyan <budakyan@gmail.com>
2022-03-22 11:34:12 +0200

    update amazing numbers

--- 08.Data types and variables/amazingNumbers.js
+++ 08.Data types and variables/amazingNumbers.js
@@ -1,5 +1,11 @@
 function amazingNumbers(num) {
-  //
+  num = num.toString();
+  let sum = 0;
+  for (let i = 0; i < num.length; i++) {
+    sum += Number(num[i]);
+  }
+  let result = sum.toString().includes("9");
+  console.log(result ? `${num} Amazing? True` : `${num} Not Amazing? False`);
 }
 
 amazingNumbers(1233);
