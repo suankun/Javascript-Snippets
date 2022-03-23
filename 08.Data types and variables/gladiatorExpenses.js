7fd41074fd8c96b46a49d7f0673da03e49c13588
Edmont Budakyan <budakyan@gmail.com>
2022-03-23 16:11:21 +0200

    updated gladiator expenses

--- 08.Data types and variables/gladiatorExpenses.js
+++ 08.Data types and variables/gladiatorExpenses.js
@@ -1,5 +1,44 @@
 function gladiatorExpenses(lostFight, helmet, sword, shield, armor) {
-  let tatalExpenses = 0;
+  let expenses = 0;
+
+  for (let i = 1; i <= lostFight; i++) {
+    if (i % 2 == 0) {
+      expenses += helmet;
+    }
+    if (i % 3 == 0) {
+      expenses += sword;
+    }
+    if (i % 6 == 0) {
+      expenses += shield;
+    }
+    if (i % 12 == 0) {
+      expenses += armor;
+    }
+  }
+  console.log(`Gladiator expenses: ${expenses.toFixed(2)} aureus`);
 }
 
 gladiatorExpenses(7, 2, 3, 4, 5);
+gladiatorExpenses(23, 12.5, 21.5, 40, 200);
+
+// function gladiatorExpenses(lostFight, helmet, sword, shield, armor) {
+//     let expenses = 0;
+//     let shieldBreakCounter = 0;
+//     for (let i = 1; i <= lostFight; i++) {
+//       if (i % 2 == 0) {
+//         expenses += helmet;
+//       }
+//       if (i % 3 == 0) {
+//         expenses += sword;
+//       }
+//       if (i % 2 == 0 && i % 3 == 0) {
+//         expenses += shield;
+//         shieldBreakCounter++;
+//       }
+//       if (shieldBreakCounter == 2) {
+//         expenses += armor;
+//         shieldBreakCounter = 0;
+//       }
+//     }
+//     console.log(`Gladiator expenses: ${expenses.toFixed(2)} aureus`);
+//   }
