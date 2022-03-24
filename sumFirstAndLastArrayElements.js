d0672a75029072f36ba2319c27d83dbdb8f4eeb8
Edmont Budakyan <budakyan@gmail.com>
2022-03-24 09:06:56 +0200

    added arrays

---
+++ sumFirstAndLastArrayElements.js
@@ -0,0 +1,7 @@
+function sumFirstAndLastArrayElements(input) {
+  const firstElement = input[0];
+  const secondElement = input[input.length - 1];
+  console.log(firstElement + secondElement);
+}
+
+sumFirstAndLastArrayElements([20, 30, 40]);
